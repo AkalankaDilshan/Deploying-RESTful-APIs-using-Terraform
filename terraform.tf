b217b7f3133235a1b2aa13fe77f991a4aaee4e12
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-27 10:54:59 +0530

    create initial folder structure

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+
+    organization = "ZeroCloud"
+
+    workspaces {
+      name = "Deploying-RESTful-APIs-using-Terraform"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.42.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
