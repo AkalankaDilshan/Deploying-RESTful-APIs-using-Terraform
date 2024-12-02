56b128f0d3770f07584f8fbf6b482a6bdfbdd20d
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-02 15:30:40 +0530

    fix: stage errors

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -100,5 +100,16 @@ resource "aws_api_gateway_deployment" "deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev"
+  description = "deployment for API gateway"
+}
+
+resource "aws_api_gateway_stage" "dev_stage" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  stage_name    = "dev"
+  description   = "Development stage"
+  variables = {
+    "env" = "dev"
+  }
+
 }
