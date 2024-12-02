c7cd4c276f6ddaa25a180e6dadc6505232d9d7ec
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-02 14:42:37 +0530

    Fixed the API Gateway issue; closes #4

--- main.tf
+++ main.tf
@@ -13,3 +13,8 @@ module "Iam_role" {
   source    = "./modules/Iam"
   role_name = "lambdaFunctionRole"
 }
+
+module "API_gateway" {
+  source            = "./modules/api_gateway"
+  lambda_invoke_arn = module.lambda_function.invoke_arn
+}
