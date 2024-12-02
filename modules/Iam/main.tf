4cd346910ead8155ea492b131ae380066f620883
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-02 22:33:29 +0530

    fix : :beers:Internal server error 3

--- modules/Iam/main.tf
+++ modules/Iam/main.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "polices" {
   statement {
     effect    = "Allow"
     actions   = ["sts:AssumeRole"]
-    resources = "*" #["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.role_name}"]
+    resources = ["*"] #["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.role_name}"]
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
