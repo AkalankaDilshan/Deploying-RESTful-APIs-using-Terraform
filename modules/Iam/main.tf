4d801d0bb2ba2bc366e61e86041f2f1da4e63a26
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-02 15:04:38 +0530

    fix : lambda permission issue

--- modules/Iam/main.tf
+++ modules/Iam/main.tf
@@ -20,3 +20,11 @@ resource "aws_iam_policy_attachment" "lambda_basic_execution" {
   roles      = [aws_iam_role.lambda_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPI/gateway"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
+}
--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,5 +1,5 @@
-output "lambda_arn" {
-  value = aws_lambda_function.Invoke_function.arn
+output "lambda_name" {
+  value = aws_lambda_function.Invoke_function.function_name
 }
 
 output "invoke_arn" {
